25859b9f0ee34f7dd96eacfb70f76be0c6ae6255
sussycylinder <115450379+sussycylinder@users.noreply.github.com>
2022-11-07 14:19:28 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.temperature())
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(input.temperature() * 1.8 + 32)
+})
